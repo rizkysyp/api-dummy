4a5a34ae50fe69eb57b0cd41916fbefc3f6a5e17
rizkysyp <rizkysyahputra14045@gmail.com>
2023-07-12 15:00:57 +0700

    Add cloudinary

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
+var cloudinary = require('cloudinary').v2;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: 'dtow6mgju',
+  api_key: '259468891937896',
+  api_secret: 'O7JrZxKF4Q13x0Y_dqnrOhKcnsE'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +36,9 @@ const data = [
   { id: 5,id_pelanggan: 5555, nama: "Rizky S", status: 1 ,kota: "batam",phoneNumber : "2020228"},
   // tambahkan data lain di sini
 ];
+
+// File upload middleware
+const upload = multer({ dest: 'uploads/' });
 
 app.get('/', (req, res) => {
   res.json(data);
@@ -53,6 +65,33 @@ app.get('/phone/:id', (req, res) => {
     res.status(404).json({ message: 'Data not found' });
   }
 });
+
+// Image upload API
+app.post('/upload', upload.single(), (req, res) => {
+  if (!req.file) {
+    res.status(400).json({ message: 'No image file found' });
+    return;
+  }
+
+  // Access the uploaded file through req.file
+  const { path, originalname } = req.file;
+
+  // Upload the file to Cloudinary
+  cloudinary.uploader.upload(path, { folder: 'uploads' }, (error, result) => {
+    if (error) {
+      console.log('Error uploading image:', error);
+      res.status(500).json({ message: 'Failed to upload image' });
+    } else {
+      // Log the Cloudinary response to the console
+      console.log('Cloudinary response:', result);
+      res.json({ message: 'Image uploaded successfully', url: result.secure_url });
+    }
+
+    // Remove the temporary file from the server
+    fs.unlinkSync(path);
+  });
+});
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
